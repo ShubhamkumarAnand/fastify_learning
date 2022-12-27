30b970a6b6838d7b14576d6076af5168d47320ec
imskanand <the.anand3099@gmail.com>
2022-12-27 16:56:21 +0530

    GET Route is complete

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import Fastify from 'fastify'
-import users from './users.json'
+import users from './users.json' assert {type: 'json'}
 
 const fastify = Fastify({
   logger: true
 })
- 
-fastify.get('/getUsers', (request, reply) => {
-  return users;
+
+fastify.get('/getUsers', async(request, reply) => {
+  return await users;
 })
 
 const PORT = process.env.PORT || 8000
