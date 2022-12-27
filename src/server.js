90d6980fd072fd565488c6bab9b9e5d2b3c273c8
imskanand <the.anand3099@gmail.com>
2022-12-27 17:27:26 +0530

    Query Parameters using TC

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import Fastify from 'fastify'
 import users from './users.json' assert {type: 'json'}
 
 const fastify = Fastify({
-  logger: true
+  logger: false
 })
 
-fastify.get('/getUsers', async(request, reply) => {
-  return await users;
+fastify.get('/getUsers',async (request, reply) => {
+  const { gender } = await request.query
+  if(!gender) return users
+  const filteredUsers = users.filter((user) => user.gender.toLowerCase() === gender.toLowerCase())
+  return filteredUsers
 })
 
 const PORT = process.env.PORT || 8000
